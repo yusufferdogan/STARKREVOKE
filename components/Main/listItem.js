f30d46fa14dfba48f83ea0b8b5a92c41cff7ae07
yusufferdogan <yusuffx03@gmail.com>
2023-07-30 17:27:31 +0300

    add token spenders name

--- components/Main/listItem.js
+++ components/Main/listItem.js
@@ -24,6 +24,7 @@ const currency = {
   '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8': 'USDT',
   '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3': 'DAI',
   '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac': 'WBTC',
+  '0x042b8f0484674ca266ac5d08e4ac6a3fe65bd3129795def2dca5c34ecc5f96d2': 'wstETH'
 };
 const icon = {
   '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7':
@@ -43,6 +44,23 @@ const decimal = {
   '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8': 6,
   '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3': 18,
   '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac': 8,
+};
+const name = {
+  '0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023':
+    'JediSwap: AMM Swap',
+  '0x07a6f98c03379b9513ca84cca1373ff452a7462a3b61598f0af5bb27ad7f76d1':
+    '10KSwap: AMM Router',
+  '0x04270219d365d6b017231b52e92b3fb5d7c8378b05e9abc97724537a80e93b0f':
+    'AVNU: Exchange',
+  '0x010884171baf1914edc28d7afb619b40a4051cfae78a094a55d230f19e944a28':
+    'mySwap: AMM Swap',
+  '0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05':
+    'zkLend: Market',
+  '0x028c858a586fa12123a1ccb337a0a3b369281f91ea00544d0c086524b759f627':
+    'SithSwap: AMM Router',
+  '0x01b23ed400b210766111ba5b1e63e33922c6ba0c45e6ad56ce112e5f4c578e62':
+    'Fibrous Finance',
+  // "" : "",
 };
 export function ListItemERC20({ transaction }) {
   const { address, status } = useAccount();
@@ -101,8 +119,8 @@ export function ListItemERC20({ transaction }) {
       <td className="px-6 py-4 text-white">
         {BigInt(num) >= UINT_256_MAX
           ? 'unlimited'
-          : parseFloat(ethers.utils.formatEther(num)) > 100000
-          ? '> 100000'
+          : parseFloat(ethers.utils.formatEther(num)) > 1000
+          ? '> 1000'
           : parseFloat(ethers.utils.formatEther(num)) < 0.0001
           ? '< 0.0001'
           : decimal[transaction.contractAddress]
@@ -125,12 +143,14 @@ export function ListItemERC20({ transaction }) {
       </td>
       <td className="px-6 py-4 text-white">
         <a href={`https://starkscan.co/contract/${transaction.spender}`}>
-          {transaction.spender.substring(0, 4) +
-            '...' +
-            transaction.spender.substring(
-              transaction.spender.length - 4,
-              transaction.spender.length
-            )}
+          {name[transaction.spender]
+            ? name[transaction.spender]
+            : transaction.spender.substring(0, 4) +
+              '...' +
+              transaction.spender.substring(
+                transaction.spender.length - 4,
+                transaction.spender.length
+              )}
         </a>
       </td>{' '}
       <td className="px-6 py-4 text-white">
