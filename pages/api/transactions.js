72388e3d8b709f7b85884e28c23161d0690f5fec
yusufferdogan <yusuffx03@gmail.com>
2023-07-30 17:01:48 +0300

    remove mock

--- pages/api/transactions.js
+++ pages/api/transactions.js
@@ -14,62 +14,61 @@ export default async function handler(req, res) {
 
   const data = [];
   let counter = 1;
-  let next_url = null;
   try {
-    // do {
-    //   console.log('sending req:', counter);
-    //   counter++;
-    //   const startTime = new Date().getTime();
+    do {
+      console.log('sending req:', counter);
+      counter++;
+      const startTime = new Date().getTime();
 
-    //   const params = { contract_address, limit, cursor: null };
+      const params = { contract_address, limit, cursor: null };
 
-    //   const response = await axios.get(url, {
-    //     headers,
-    //     params: params,
-    //   });
+      const response = await axios.get(url, {
+        headers,
+        params: params,
+        order_by: 'asc'
+      });
 
-    //   console.log("response.next_url:", response.next_url)
-    //   console.log("response:", response)
+      console.log("response.next_url:", response.data.next_url)
+      // console.log("response:", response)
 
-    //   next_url = response.next_url
+      if (response.data.next_url) {
+        const searching_url = new URL(response.data.next_url);
+        const searchParams = searching_url.searchParams;
+        const cursor = searchParams.get('cursor'); // 'value1'
+        if (cursor) {
+          params.cursor = cursor;
+        }
+      }
 
-    //   if (response.next_url) {
-    //     const searching_url = new URL(response.next_url);
-    //     const searchParams = searching_url.searchParams;
-    //     const cursor = searchParams.get('cursor'); // 'value1'
-    //     if (cursor) {
-    //       params.cursor = cursor;
-    //     }
-    //   }
+      for (let index = 0; index < response.data.data.length; index++) {
+        const element = response.data.data[index];
+        console.log(element.nonce);
+        data.push(element);
+      }
 
-    //   for (let index = 0; index < response.data.data.length; index++) {
-    //     const element = response.data.data[index];
-    //     console.log(element.nonce);
-    //     data.push(element);
-    //   }
+      console.log('data.length: ', data.length);
 
-    //   console.log('data.length: ', data.length);
+      const endTime = new Date().getTime();
 
-    //   const endTime = new Date().getTime();
+      // Calculate the time elapsed in milliseconds
+      const timeElapsed = endTime - startTime;
 
-    //   // Calculate the time elapsed in milliseconds
-    //   const timeElapsed = endTime - startTime;
+      console.log('Time Elapsed (ms):', timeElapsed);
 
-    //   console.log('Time Elapsed (ms):', timeElapsed);
+      console.log('response.data.data.length:', response.data.data.length);
 
-    //   console.log('response.data.data.length:', response.data.data.length);
+      if (response.data.next_url) await sleep(1000);
+      if (response.data.next_url) break;
+    } while (1);
 
-    //   if (response.next_url) await sleep(1000);
-    // } while (next_url);
-
-    // console.log('Data.length:', data.length);
+    console.log('Data.length:', data.length);
 
     // Set the Access-Control-Allow-Origin header to allow requests from any origin
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Return the resource data as a JSON response
     console.log("REQUESTED");
-    res.status(200).json(mockData.data);
+    res.status(200).json(data);
   } catch (error) {
     console.error('Error fetching resource:', error);
     res.status(500).send('Error fetching resource');
