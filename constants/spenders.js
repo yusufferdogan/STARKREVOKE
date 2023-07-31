019a39f9488d95066ff9dfcf4c922ec849e0db28
yusufferdogan <yusuffx03@gmail.com>
2023-07-31 12:45:32 +0300

    add  spender lists

---
+++ constants/spenders.js
@@ -0,0 +1,206 @@
+export const SPENDERS = [
+  {
+    contract_address:
+      '0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023',
+    class_hash:
+      '0x07197021c108b0cc57ae354f5ad02222c4b3d7344664e6dd602a0e2298595434',
+    deployed_at_transaction_hash:
+      '0x041f8d0866ce58959406b15bbd9ddcea2ed309e20dc2a76982eaeaacc192403f',
+    deployed_at_timestamp: 1665936274,
+    name_tag: 'JediSwap: AMM Swap',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x07a6f98c03379b9513ca84cca1373ff452a7462a3b61598f0af5bb27ad7f76d1',
+    class_hash:
+      '0x0514718bb56ed2a8607554c7d393c2ffd73cbab971c120b00a2ce27cc58dd1c1',
+    deployed_at_transaction_hash:
+      '0x0422f60f8078e9c8aac73c99a016e40e7485aa62c1cce21deb1de63bc5ede4c5',
+    deployed_at_timestamp: 1663557163,
+    name_tag: '10KSwap: AMM Router',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x04270219d365d6b017231b52e92b3fb5d7c8378b05e9abc97724537a80e93b0f',
+    class_hash:
+      '0x05ee939756c1a60b029c594da00e637bf5923bf04a86ff163e877e899c0840eb',
+    deployed_at_transaction_hash:
+      '0x04762bb00f9c71c748744d1d797ccd15396c22383a9fb40726e779a3322bbb64',
+    deployed_at_timestamp: 1681746641,
+    name_tag: 'AVNU: Exchange',
+    implementation_type: 'UNKNOWN',
+    is_verified: false,
+  },
+  {
+    contract_address:
+      '0x010884171baf1914edc28d7afb619b40a4051cfae78a094a55d230f19e944a28',
+    class_hash:
+      '0x07e35b811e3d4e2678d037b632a0c8a09a46d82185187762c0d429363e3ef9cf',
+    deployed_at_transaction_hash:
+      '0x019a567e560c0bdf435e29603cb15f318f67a828321fb3526823a05d6503138e',
+    deployed_at_timestamp: 1659387784,
+    name_tag: 'mySwap: AMM Swap',
+    implementation_type: 'UNKNOWN',
+    is_verified: false,
+  },
+  {
+    contract_address:
+      '0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05',
+    class_hash:
+      '0x012712b5fa73c08cb9ab175209efe37026ae3bdd8cd8bc3c8e02956505c4c8b2',
+    deployed_at_transaction_hash:
+      '0x005b1a127baaa32ae4c884640176cc0552cb28ed615be4d9a0a8cdf1a4b82984',
+    deployed_at_timestamp: 1683221114,
+    name_tag: 'zkLend: Market',
+    implementation_type: 'UNKNOWN',
+    is_verified: false,
+  },
+  {
+    contract_address:
+      '0x028c858a586fa12123a1ccb337a0a3b369281f91ea00544d0c086524b759f627',
+    class_hash:
+      '0x00cabe1e6e783267adac15159ae3dad843389eb99f3a6d0ac69ec7f22a99d164',
+    deployed_at_transaction_hash:
+      '0x0218fba8a764248d6560e0add1bd9aa6b68a22a43542a3701f0d1172d84f63f8',
+    deployed_at_timestamp: 1673367399,
+    name_tag: 'SithSwap: AMM Router',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x01b23ed400b210766111ba5b1e63e33922c6ba0c45e6ad56ce112e5f4c578e62',
+    class_hash:
+      '0x07ab5ccfc00188f7f5a2a507b42f1b452a2115709e714f2ff5c225ae341c539d',
+    deployed_at_transaction_hash:
+      '0x065209c4062d5fe8532c8831b89459deef5f09771e64bca3b198ffeead450138',
+    deployed_at_timestamp: 1685231674,
+    name_tag: 'Fibrous Finance: Router V1',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x06ac597f8116f886fa1c97a23fa4e08299975ecaf6b598873ca6792b9bbfb678',
+    class_hash:
+      '0x01067c8f4aa8f7d6380cc1b633551e2a516d69ad3de08af1b3d82e111b4feda4',
+    deployed_at_transaction_hash:
+      '0x078227cb82c7967e8ca6274b403b7c98f5c7bd8b3a2a55f4076dc9b17ac2af72',
+    deployed_at_timestamp: 1669720110,
+    name_tag: 'Starknet.id: Naming',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x079b882cb8200c1c1d20e849a2ef19124b0b8985358c1313ea6af588cfe4fec8',
+    class_hash:
+      '0x05c748a01c1f7c05712b12716d1a14e03844bd92e3ffe2624d12185d30a4912c',
+    deployed_at_transaction_hash:
+      '0x04c09c5febd675df2e7a66f4746f0ec3408844a856d749be59e10c0cc1033465',
+    deployed_at_timestamp: 1668654822,
+    name_tag: 'Mint Square: NFT Marketplace',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x076dbabc4293db346b0a56b29b6ea9fe18e93742c73f12348c8747ecfc1050aa',
+    class_hash:
+      '0x00eafb0413e759430def79539db681f8a4eb98cf4196fe457077d694c6aeeb82',
+    deployed_at_transaction_hash:
+      '0x04d39840c3281a186990e69c3e4a7bda335b117040cdc0be3e5df09ea8eecdf9',
+    deployed_at_timestamp: 1680864820,
+    name_tag: 'Carmine Options: AMM',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x0346c57f094d641ad94e43468628d8e9c574dcb2803ec372576ccc60a40be2c4',
+    class_hash:
+      '0x0639a9186c8d9daa7c44186e7cf987cc458a46965e2658d27020554f9671677b',
+    deployed_at_transaction_hash:
+      '0x01f79718953efe215bfaaffed6686f2469609200e7f9f7e9d803d6da39c55203',
+    deployed_at_timestamp: 1669216655,
+    name_tag: 'Pragma: OracleProxy',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x02a92f0f860bf7c63fb9ef42cff4137006b309e0e6e1484e42d0b5511959414d',
+    class_hash:
+      '0x071891339979b295508cd61a8477959484b955c08a617cec54ade3ccedf63762',
+    deployed_at_transaction_hash:
+      '0x0793dea58e9bc2c6fa30cce54698e0a19414a8ba8edcba61ceb2ca91c54bd545',
+    deployed_at_timestamp: 1658959893,
+    name_tag: 'Aspect: NFT Marketplace',
+    implementation_type: 'UNKNOWN',
+    is_verified: false,
+  },
+  {
+    contract_address:
+      '0x0173f81c529191726c6e7287e24626fe24760ac44dae2a1f7e02080230f8458b',
+    class_hash:
+      '0x068bb00f783b88aeb61551f5383e6b7f1621463cb570e8f63df89e3681045134',
+    deployed_at_transaction_hash:
+      '0x05c3c54f56333fcd78921d387edf346fd475710a56237bae3575bae942c99ec2',
+    deployed_at_timestamp: 1655953006,
+    name_tag: 'Orbiter Finance',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x029a303b928b9391ce797ec27d011d3937054bee783ca7831df792bae00c925c',
+    class_hash:
+      '0x0064aaf619d5b32b5496cb17d64ad38f390598442d6ac9a2d72c6280b863efba',
+    deployed_at_transaction_hash:
+      '0x0727c2b13e6a4a883414b7f973a92c343512d5efa38e693fbc7978e01894416a',
+    deployed_at_timestamp: 1669029090,
+    name_tag: 'JediSwap: Zapper',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x063a4b3b0122cdaa6ba244739add94aed1d31e3330458cda833a8d119f28cbe8',
+    class_hash:
+      '0x04d1f4cf4ef520c768a326d34f919227e1f075effda532f57cbaec6a1228db88',
+    deployed_at_transaction_hash:
+      '0x039167f6fa47b5f3d9b8fb372c7cf83f338734cfccbdae9e95dba975dfa5a387',
+    deployed_at_timestamp: 1663677711,
+    name_tag: 'Rules: Marketplace',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x074761a8d48ce002963002becc6d9c3dd8a2a05b1075d55e5967f42296f16bd0',
+    class_hash:
+      '0x052c7ba99c77fc38dd3346beea6c0753c3471f2e3135af5bb837d6c9523fff62',
+    deployed_at_transaction_hash:
+      '0x02b4bafecadacd3c490f2f08b2644f7f268193005d517f2ef8e1b30da40faf69',
+    deployed_at_timestamp: 1657094995,
+    name_tag: 'StarkGate: USDT Bridge',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+  {
+    contract_address:
+      '0x04767b873669406d25dddbf67356e385a14480979e5358a411955d692576aa30',
+    class_hash:
+      '0x05c748a01c1f7c05712b12716d1a14e03844bd92e3ffe2624d12185d30a4912c',
+    deployed_at_transaction_hash:
+      '0x05ba36db10071b9471ddecf0b4440f6e914bbc759dfea38c2211a2243756df78',
+    deployed_at_timestamp: 1667350259,
+    name_tag: 'Mint Square: NFT Marketplace Manager',
+    implementation_type: 'UNKNOWN',
+    is_verified: true,
+  },
+];
