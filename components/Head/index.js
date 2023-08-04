8c1a667b27b9085182f6f27d2c9cdfeb67bfaf88
yusufferdogan <yusuffx03@gmail.com>
2023-08-04 15:35:47 +0300

    update head

--- components/Head/index.js
+++ components/Head/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Head from 'next/head';
+// import { useAccount } from '@starknet-react/core';
 
+// function substr(str) {
+//   return (
+//     str.substring(0, 6) + '...' + str.substring(str.length - 4, str.length)
+//   );
+// }
 export function CustomHead(params) {
+  // const { account, address, status } = useAccount();
+  //{address ? ' | ' + substr(address) : ''}
   return (
     <Head>
-      <title>Revoke.Starknet</title>
-      <meta name="description" content="REVOKE.STARKNET" />
+      <title>StarkRevoke - Starknet Approve Revoker </title>
+      <meta name="description" content="starkrevoke.com" />
       <link rel="icon" href="favicon.ico" />
     </Head>
   );
