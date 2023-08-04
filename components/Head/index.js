d20224fc7d85a63669e0ec99d6d70330da333001
yusufferdogan <yusuffx03@gmail.com>
2023-08-04 15:54:54 +0300

    update metadata

--- components/Head/index.js
+++ components/Head/index.js
@@ -14,6 +14,23 @@ export function CustomHead(params) {
     <Head>
       <title>StarkRevoke - Starknet Approve Revoker </title>
       <meta name="description" content="starkrevoke.com" />
+      <meta
+        name="keywords"
+        content="Starknet Revoke Allowance Approve Cairo Zk Rust"
+      />
+      <meta name="author" content="Yusuf Erdogan" />
+
+      <meta property="og:title" content="StarkRevoke.com" />
+      <meta
+        property="og:description"
+        content="StarkRevoke - Starknet Approve Revoker"
+      />
+      <meta
+        property="og:image"
+        content="https://images.emojiterra.com/twitter/512px/1f6e1.png"
+      />
+      <meta property="og:url" content="https://www.starkrevoke.com/" />
+      <meta property="og:type" content="website" />
       <link rel="icon" href="favicon.ico" />
     </Head>
   );
