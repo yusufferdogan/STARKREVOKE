e29d78e69836928dcdd2a1188551b3f4eac2b4d6
yusufferdogan <yusuffx03@gmail.com>
2023-09-15 15:03:06 +0300

    change revoke all button color to green

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,5 +1,4 @@
 import { React, useEffect, useState } from 'react';
-import 'tailwindcss/tailwind.css';
 import { ListItemERC20 } from './listItem';
 import { ListItemERC721 } from './listItemErc721';
 import { IoLogoGithub } from 'react-icons/io';
@@ -161,11 +160,11 @@ function Home() {
                         .length === 0
                     }
                     type="button"
-                    className={`focus:outline-none text-white ${
+                    className={`focus:outline-none text-black ${
                       Object.values(selected).filter((value) => value === true)
                         .length === 0
                         ? 'bg-gray-300 text-black text-sm px-5 py-2.5 mr-2 mb-2'
-                        : 'bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
+                        : 'bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'
                     } font-medium rounded-lg`}
                   >
                     REVOKE ALL
